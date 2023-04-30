2f416eb777f177708544dfda6d580e46ac13fe38
Shaun Becker <Shaun.Becker@marcusmillichap.com>
2023-04-30 16:12:24 -0700

    Test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone"
+  output: "standalone",
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, './'),
+    outputFileTracingIncludes: {
+      '/eslint-config-custom': ['./eslint-config-custom/**/*'],
+      '/tsconfig': ['./tsconfig/**/*'],
+      '/ui': ['./ui/**/*'],
+    }
+  },
 }
 
 module.exports = nextConfig
