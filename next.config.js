6e95d242d692296580434c3ed4082f9765a9c9f2
Shaun Becker <Shaun.Becker@marcusmillichap.com>
2023-04-30 20:53:16 -0700

    Revert

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
-  experimental: {
-    // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, './'),
-    outputFileTracingIncludes: {
-      '/eslint-config-custom': ['./eslint-config-custom/**/*'],
-      '/tsconfig': ['./tsconfig/**/*'],
-      '/ui': ['./ui/**/*'],
-    }
-  },
+  output: "standalone"
 }
 
 module.exports = nextConfig
